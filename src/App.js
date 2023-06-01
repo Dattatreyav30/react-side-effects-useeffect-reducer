5370b5df62b2fba2bea61468e34973574f5e44a7
dattatreyav30 <dattatreya361@gmail.com>
2023-06-01 23:34:55 +0530

    using context in easier way sinated of returning in anonymous functionr

--- src/App.js
+++ src/App.js
@@ -28,11 +28,13 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {isLoggedIn && <Home onLogout ={logoutHandler}/>}
       </main>
     </AuthContext.Provider>
   );
